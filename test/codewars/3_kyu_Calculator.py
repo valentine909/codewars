acc2797aa6de603d216565bf954f3227356a2fb2
Valentine <valentine.nazarov@gmail.com>
2019-12-19 22:47:55 +0300

    v0.3

--- test/codewars/3_kyu_Calculator.py
+++ test/codewars/3_kyu_Calculator.py
@@ -1,36 +1,59 @@
+import operator
 import re
-pattern_addition = re.compile(r"\d+\s\+\s\d+")
-pattern_subtraction = re.compile(r"\d+\s\-\s\d+")
-pattern_multiplication = re.compile(r"\d+\s\*\s\d+")
-pattern_division = re.compile(r"\d+\s/\s\d+")
+pattern_addition = re.compile(r"[0-9.,]+\s\+\s[0-9.,]+")
+pattern_subtraction = re.compile(r"[0-9.,]+\s\-\s[0-9.,]+")
+pattern_multiplication = re.compile(r"[0-9.,]+\s\*\s[0-9.,]+")
+pattern_division = re.compile(r"[0-9.,]+\s/\s[0-9.,]+")
 pattern_brackets = re.compile(r"\([^\(\)]+\)")
+lib = [[pattern_multiplication, ' * ', operator.mul], [pattern_division, ' / ', operator.truediv],
+       [pattern_addition, ' + ', operator.add], [pattern_subtraction, ' - ', operator.sub]]
 
 
 class Calculator(object):
   def evaluate(self, string):
-    return 0
-
-
-a = "((1254 + 3) * 2 / 2 + 3) * (4 + 5) / 6"
-b = "1254 + 3 * 2 / 2 + 3 * 4"
+    return float(find_brackets(string))
 
 
 def find_brackets(string):
     m = re.findall(pattern_brackets, string)
     for i in m:
-        a = solve(m[0][1:len(m[0]) - 1])
+        result = solve(i[1:len(i) - 1])
+        string = string.replace(i, str(result)).replace('--', '+')
     if m:
-        find_brackets(string)
+        string = find_brackets(string)
+    else:
+        return solve(string)
+    return string
 
 
 def solve(substring):
-    if '/' in substring:
-        digits = substring.split(" / ")
-    print(float(digits[0]) / float(digits[1]))
+    for i in lib:
+        while True:
+            m = re.findall(i[0], substring)
+            if not m:
+                break
+            else:
+                digits = m[0].split(i[1])
+                result = i[2](float(digits[0]), float(digits[1]))
+                substring = substring.replace(m[0], str(result)).replace('--', '+')
+    return substring
 
 
+a = "((1254 + 3) * 2 / 2 + 3) * (4 + 5) / 6"
+b = "1254 + 3 * 2 / 2 + 3 * 4"
+c = "2 - 3 - 4 * -5"
 
 p = re.findall(pattern_brackets, a)
+print(p)
 print(p[0][1:len(p[0]) - 1])
 
-solve("2 / 5")
+print(eval(b))
+print(solve(b))
+
+print(eval(a))
+print(find_brackets(a))
+
+print(eval(c))
+print(find_brackets(c))
+
+print(c.split(' * '))
