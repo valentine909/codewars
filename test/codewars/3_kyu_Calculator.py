6bf931341e10ebb527bdba4604d5e8842d5e37e5
Valentine <valentine.nazarov@gmail.com>
2019-12-20 22:37:05 +0300

    two stages in pattern analysis

--- test/codewars/3_kyu_Calculator.py
+++ test/codewars/3_kyu_Calculator.py
@@ -5,8 +5,9 @@ pattern_subtraction = re.compile(r"\-?[0-9.,]+\s\-\s\-?[0-9.,]+")
 pattern_multiplication = re.compile(r"\-?[0-9.,]+\s\*\s\-?[0-9.,]+")
 pattern_division = re.compile(r"\-?[0-9.,]+\s/\s\-?[0-9.,]+")
 pattern_brackets = re.compile(r"\([^\(\)]+\)")
-lib = [[pattern_multiplication, ' * ', operator.mul], [pattern_division, ' / ', operator.truediv],
-       [pattern_addition, ' + ', operator.add], [pattern_subtraction, ' - ', operator.sub]]
+lib1 = {"*": [pattern_multiplication, ' * ', operator.mul], "/": [pattern_division, ' / ', operator.truediv]}
+lib2 = {"+": [pattern_addition, ' + ', operator.add], "-": [pattern_subtraction, ' - ', operator.sub]}
+libs = [lib1, lib2]
 
 
 class Calculator(object):
@@ -27,15 +28,17 @@ def find_brackets(string):
 
 
 def solve(substring):
-    for i in lib:
-        while True:
-            m = re.findall(i[0], substring)
-            if not m:
-                break
-            else:
-                digits = m[0].split(i[1])
-                result = i[2](float(digits[0]), float(digits[1]))
-                substring = substring.replace(m[0], str(result)).replace('--', '+')
+    for lib in libs:
+        for i in substring:
+            if i in lib:
+                while True:
+                    m = re.findall(lib[i][0], substring)
+                    if not m:
+                        break
+                    else:
+                        digits = m[0].split(lib[i][1])
+                        result = lib[i][2](float(digits[0]), float(digits[1]))
+                        substring = substring.replace(m[0], str(result)).replace('--', '+')
     return substring
 
 
