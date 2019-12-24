3a0f8a274eda9c9739d46223b7c822acae94c241
Valentine Nazarov <valentine.nazarov@gamil.com>
2019-12-24 22:40:02 +0300

    better algorithm

--- test/codewars/4_kyu_Strip_Comments.py
+++ test/codewars/4_kyu_Strip_Comments.py
@@ -3,12 +3,9 @@ https://www.codewars.com/kata/strip-comments/train/python
 """
 
 
-def solution(a, symbols):
-    for i in symbols:
-        while i in a:
-            x, y = a.split(i, 1)
-            if "\n" in y:
-                a = x.strip(" ") + "\n" + y.split("\n", 1)[1]
-            else:
-                a = x.strip(" ")
-    return a
+def solution(string, symbols):
+    lst = string.split("\n")
+    for symbol in symbols:
+        for i in range(len(lst)):
+            lst[i] = lst[i].split(symbol)[0].rstrip()
+    return "\n".join(lst)
